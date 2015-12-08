3db063eed2c3f529538e29b2f4e939e11c19903f
nwshane <django09@gmail.com>
2015-12-08 17:58:15 +0400

    Factored out separate function to find word info from demo words.

--- modules/fake_words_api/search.js
+++ modules/fake_words_api/search.js
@@ -10,15 +10,16 @@ function find_related_words(search_word_info) {
   return related_words
 }
 
-function fake_words_api_search(search_word) {
-  // go through words to find the word
-  // get lemma
-  // find all words with the same lemma
-  // get em all in an array
-  // return the array
+function find_word_info(search_word) {
   let search_word_info = words.filter(function(word) {
     return (word['word'] == search_word);
   })[0];
+
+  return search_word_info;
+}
+
+function fake_words_api_search(search_word) {
+  let search_word_info = find_word_info(search_word);
 
   return {
     search_word: search_word_info,
