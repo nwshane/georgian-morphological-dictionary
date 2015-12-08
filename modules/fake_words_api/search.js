1bec4360171f5ecd60cfe00c5c36fac38d8e0ee0
nwshane <django09@gmail.com>
2015-12-08 16:22:35 +0400

    Moved access to demo words and details of demo words search into a fake API.

---
+++ modules/fake_words_api/search.js
@@ -0,0 +1,22 @@
+import { words } from './demo_words';
+
+function fake_words_api_search(search_word) {
+  // go through words to find the word
+  // get lemma
+  // find all words with the same lemma
+  // get em all in an array
+  // return the array
+  let search_word_info = words.filter(function(word) {
+    return (word['word'] == search_word);
+  });
+
+  let search_word_lemma = search_word_info[0]['lemma'];
+
+  let related_words = words.filter(function(word) {
+    return (word['word'] == search_word_lemma || word['lemma'] == search_word_lemma);
+  })
+
+  return [search_word_info, related_words];
+}
+
+export { fake_words_api_search };
--- modules/index.js
+++ modules/index.js
@@ -1,22 +1,7 @@
-import { words } from './demo_words';
+import { fake_words_api_search } from './fake_words_api/search.js';
 
 function search_for_word(search_word) {
-  // go through words to find the word
-  // get lemma
-  // find all words with the same lemma
-  // get em all in an array
-  // return the array
-  let search_word_info = words.filter(function(word) {
-    return (word['word'] == search_word);
-  });
-
-  let search_word_lemma = search_word_info[0]['lemma'];
-
-  let related_words = words.filter(function(word) {
-    return (word['word'] == search_word_lemma || word['lemma'] == search_word_lemma);
-  })
-
-  return [search_word_info, related_words];
+  return fake_words_api_search(search_word);
 }
 
 function display_word_info(word_results) {
