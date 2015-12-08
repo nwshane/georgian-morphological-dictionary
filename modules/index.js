aed59a4d1af9805c2461daefd1e5543fce42453a
nwshane <django09@gmail.com>
2015-12-08 15:39:49 +0400

    Displaying searched for word on home page.

--- modules/index.js
+++ modules/index.js
@@ -14,11 +14,15 @@ function search_for_word(search_word) {
   return selected_words;
 }
 
+function display_words(search_word, selected_words) {
+  $('.js-fill-with-search-results').html('<h2>' + search_word + '</h2>')
+}
+
 function bind_word_search_to_search_input() {
   $('.js-search-for-word').change(function() {
       let search_word = $(this).val();
       let selected_words = search_for_word(search_word);
-      console.log(selected_words);
+      display_words(search_word, selected_words)
   });
 }
 
