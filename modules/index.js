69e004d6c10531b16c68cbdd12f63a91a7604831
nwshane <django09@gmail.com>
2015-12-08 15:28:26 +0400

    Added simple search input to search for words in demo_words array.

--- modules/index.js
+++ modules/index.js
@@ -1,16 +1,25 @@
 import { words } from './demo_words';
 
-function search(word) {
+function search_for_word(search_word) {
   // go through words to find the word
   // get lemma
   // find all words with the same lemma
   // get em all in an array
   // return the array
-  let selected_words = words.filter(function(x) {
-    return (x['word'] == word);
+  let selected_words = words.filter(function(word) {
+    return (word['word'] == search_word);
   });
-  debugger;
+
+  console.log(selected_words)
   return selected_words;
 }
 
-search('არის');
+$(document).ready(function() {
+
+  $('.js-search-for-word').change(function() {
+      let search_word = $(this).val();
+      let selected_words = search_for_word(search_word);
+      console.log(selected_words);
+  });
+
+});
