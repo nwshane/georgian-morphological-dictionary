8c93e3d58c673e524b78ebe266e80e36a48fa6c7
nwshane <django09@gmail.com>
2015-12-08 16:05:14 +0400

    Expanded search input to get words that have the same lemma.

--- modules/index.js
+++ modules/index.js
@@ -6,23 +6,29 @@ function search_for_word(search_word) {
   // find all words with the same lemma
   // get em all in an array
   // return the array
-  let selected_words = words.filter(function(word) {
+  let search_word_info = words.filter(function(word) {
     return (word['word'] == search_word);
   });
 
-  console.log(selected_words)
-  return selected_words;
+  let search_word_lemma = search_word_info[0]['lemma'];
+
+  let related_words = words.filter(function(word) {
+    return (word['word'] == search_word_lemma || word['lemma'] == search_word_lemma);
+  })
+
+  return [search_word_info, related_words];
 }
 
-function display_words(search_word, selected_words) {
-  $('.js-fill-with-search-results').html('<h2>' + search_word + '</h2>')
+function display_word_info(word_results) {
+  let search_word_info = word_results[0];
+  let related_words = word_results[1];
+  $('.js-fill-with-search-results').html('<h2>' + search_word_info['word'] + '</h2>')
 }
 
 function bind_word_search_to_search_input() {
   $('.js-search-for-word').change(function() {
       let search_word = $(this).val();
-      let selected_words = search_for_word(search_word);
-      display_words(search_word, selected_words)
+      display_word_info(search_for_word(search_word));
   });
 }
 
