42f4cbf0286c4a0aee605c76165a05e5d23dfd6d
nwshane <django09@gmail.com>
2015-12-14 18:56:39 +0400

    Added pending steps for word type features.

---
+++ features/step_definitions/search_for_word_type.steps.js
@@ -0,0 +1,25 @@
+'use strict';
+
+module.exports = function() {
+
+  this.Given(/^I am on the search page$/, function (callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback.pending();
+  });
+
+  this.When(/^I search for გაკეთება$/, function (callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback.pending();
+  });
+
+  this.Then(/^I should see "([^"]*)" in the results$/, function (arg1, callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback.pending();
+  });
+
+  this.When(/^I search for აკეთებს$/, function (callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback.pending();
+  });
+
+};
