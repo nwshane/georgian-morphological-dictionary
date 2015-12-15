ad4205e00f4059107845b704c8c7dbd16cc376ba
nwshane <django09@gmail.com>
2015-12-15 17:25:08 +0400

    Renamed some search functions for more accurate descriptions of their roles.

--- modules/index.js
+++ modules/index.js
@@ -1,7 +1,7 @@
 import { fake_words_api_search } from './fake_words_api/search.js';
 import { search_results_container } from './search_results_container.js';
 
-function search_for_word(search_word) {
+function perform_search(search_word) {
   let response = fake_words_api_search(search_word);
 
   return response;
@@ -17,17 +17,17 @@ function get_search_results_from_response(response) {
   return response['search_results'];
 }
 
-function search_for_word_and_display_results(search_word_text) {
-  let response = search_for_word(search_word_text);
+function search_and_display_results(search_query) {
+  let response = perform_search(search_query);
   let search_results = get_search_results_from_response(response);
-  let search_word = filter_word_by_text(search_results, search_word_text);
+  let search_word = filter_word_by_text(search_results, search_query);
 
   search_results_container.fill(search_word, search_results);
 }
 
 function bind_word_search_to_search_input() {
   $('.js-search-for-word').change(function() {
-    search_for_word_and_display_results($(this).val());
+    search_and_display_results($(this).val());
   });
 }
 
