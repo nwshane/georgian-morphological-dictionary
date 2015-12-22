16abe8af47b7c81d9fd8630781c3068c0e764393
nwshane <django09@gmail.com>
2015-12-22 16:09:11 +0400

    Code style fix.

--- modules/searcher.js
+++ modules/searcher.js
@@ -1,6 +1,6 @@
+import { search_input } from './search_input.js';
 import { search_results_processor } from './search_results_processor.js';
 import { search_results_container } from './search_results_container.js';
-import { search_input } from './search_input.js';
 
 $(document).ready(function() {
   search_input.bind_to_search_method(
