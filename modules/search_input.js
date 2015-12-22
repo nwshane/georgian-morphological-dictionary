1bc848287e522c73686b9411f4600fd6af18aa3a
nwshane <django09@gmail.com>
2015-12-22 16:00:37 +0400

    Moved perform_search method to search_input object.

--- modules/search_input.js
+++ modules/search_input.js
@@ -1,11 +1,19 @@
+import { fake_words_api_search } from './fake_words_api/search.js';
+
 let search_input = {
 
   selector: '.js-perform-search',
 
-  bind_to_search_method: function(perform_search, search_results_processor, search_results_container) {
+  perform_search: function(search_word) {
+    return fake_words_api_search(search_word);
+  },
+
+  bind_to_search_method: function(search_results_processor, search_results_container) {
+    let that = this;
+
     $(this.selector).change(function() {
       let search_query = this.value;
-      let response = perform_search(search_query);
+      let response = that.perform_search(search_query);
 
       let processed_results = search_results_processor.process_response(response, search_query);
 
--- modules/searcher.js
+++ modules/searcher.js
@@ -1,15 +1,9 @@
-import { fake_words_api_search } from './fake_words_api/search.js';
 import { search_results_processor } from './search_results_processor.js';
 import { search_results_container } from './search_results_container.js';
 import { search_input } from './search_input.js';
 
-function perform_search(search_word) {
-  return fake_words_api_search(search_word);
-}
-
 $(document).ready(function() {
   search_input.bind_to_search_method(
-    perform_search,
     search_results_processor,
     search_results_container
   );
