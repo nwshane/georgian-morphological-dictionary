0893956ab84880d067fb2ecbfff0c2bd32627a43
nwshane <django09@gmail.com>
2016-01-15 18:21:13 +0400

    Added verb section to search_results_container template.

--- modules/searcher/search_results_container.js
+++ modules/searcher/search_results_container.js
@@ -2,15 +2,28 @@ let search_results_container = {
 
   selector: '.js-fill-with-search-results',
 
-  template:
-  "<h2 class='heading mod-search-results'>{{search_word_text}}</h2>\
-  <section>\
-    <h3>Word Info</h3>\
-    <p>Type: {{search_word_type}}</p>\
-  </section>",
+  template_section_verbs:
+    '<section>\
+      <h3>Verbs</h3>\
+      <section>\
+        <h4>Present Tense</h4>\
+        <ul>\
+          <li>Hello</li>\
+        </ul>\
+      </section>\
+    </section>',
+
+  template: function() {
+    return "<h2 class='heading mod-search-results'>{{search_word_text}}</h2>\
+      <section>\
+        <h3>Word Info</h3>\
+        <p>Type: {{search_word_type}}</p>\
+      </section>" +
+      this.template_section_verbs;
+  },
 
   compile_results_into_html: function(results) {
-    let template = Handlebars.compile(this.template);
+    let template = Handlebars.compile(this.template());
     return template(results);
   },
 
