83175b502a10256864432c8e54f4d534db9644a3
nwshane <django09@gmail.com>
2016-01-18 17:45:56 +0400

    Replaced use of custom Handlebars helpers with built in #each method.

--- modules/searcher/search_results_container.js
+++ modules/searcher/search_results_container.js
@@ -2,31 +2,6 @@ let search_results_container = {
 
   selector: '.js-fill-with-search-results',
 
-  generate_list_from_array: function(array, get_content) {
-    var html = "<ul>";
-
-    for (let i = 0; i < array.length; i++) {
-      html = html + "<li>" + get_content(array[i]) + "</li>";
-    }
-
-    return html + "</ul>";
-  },
-
-  initialize: function() {
-    let that = this;
-
-    Handlebars.registerHelper('list', function(context, options) {
-      return that.generate_list_from_array(context, options.fn);
-    });
-
-    Handlebars.registerHelper('verb_block', function(context, options) {
-      let html = '<h4>' + context.tense + '</h4>';
-      html += that.generate_list_from_array(context.words, options.fn);
-
-      return html;
-    });
-  },
-
   template: function() {
     return $('#js-template-search-results').html();
   },
