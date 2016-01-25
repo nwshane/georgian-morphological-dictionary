e9afaf93539c93d0bf785b87ea4b5d9ee1908f4e
nwshane <django09@gmail.com>
2016-01-25 16:51:06 +0400

    Fixed search for words with undefined or '???' lemmas.

--- modules/fake_words_api/search.js
+++ modules/fake_words_api/search.js
@@ -1,21 +1,54 @@
 import { words } from './demo_words';
 
-function find_related_words(search_word_info) {
-  let search_word_lemma = search_word_info['lemma'];
+// Removes duplicates in array
+// Source: http://stackoverflow.com/questions/1584370/how-to-merge-two-arrays-in-javascript-and-de-duplicate-items
+Array.prototype.unique = function() {
+    var a = this.concat();
+    for(var i=0; i<a.length; ++i) {
+        for(var j=i+1; j<a.length; ++j) {
+            if(a[i] === a[j])
+                a.splice(j--, 1);
+        }
+    }
 
-  let related_words = words.filter(function(word) {
-    return (word['text'] == search_word_lemma || word['lemma'] == search_word_lemma);
+    return a;
+};
+
+function find_words_with_lemma(lemma) {
+  return words.filter(function(word) {
+    return (word['lemma'] === lemma);
   });
+}
+
+function concat_words_related_by_lemma(array, lemma_text) {
+  let lemma = find_word_by_text(search_word['lemma']);
+
+  let related_words = [lemma].concat(find_words_with_lemma(lemma['text']));
+
+  return array.concat()
+}
+
+function find_related_words(search_word) {
+  let related_words = [search_word];
+  related_words = related_words.concat(find_words_with_lemma(search_word['text']));
+
+  if (search_word['lemma'] === undefined || search_word['lemma'] === '???') {
+    return related_words;
+  }
+
+  let lemma_of_search_word = find_word_by_text(search_word['lemma'])
+
+  related_words.push(lemma_of_search_word);
+
+  related_words = related_words.concat(find_words_with_lemma(lemma_of_search_word['text'])).unique();
 
   return related_words;
 }
 
-function find_word_info(search_word) {
-  let search_word_info = words.filter(function(word) {
-    return (word['text'] == search_word);
-  })[0];
-
-  return search_word_info;
+function find_word_by_text(word_text) {
+  return words.filter(function(word) {
+    return (word['text'] == word_text);
+  })[0];;
 }
 
 function filter_by_tense(tense, words) {
@@ -64,14 +97,14 @@ function organize_words(words) {
   return organized_words;
 }
 
-function fake_words_api_search(search_word) {
-  let search_word_info = find_word_info(search_word);
+function fake_words_api_search(search_query) {
+  let search_word = find_word_by_text(search_query);
 
-  let words = find_related_words(search_word_info);
+  let words = find_related_words(search_word);
   words = organize_words(words);
 
   return {
-    search_word: search_word_info,
+    search_word: search_word,
     search_results: words
   };
 }
