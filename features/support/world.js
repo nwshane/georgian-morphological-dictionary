f76f68c0e3f9288ea8bf990d373587cbeac255ec
nwshane <django09@gmail.com>
2016-01-25 17:36:13 +0400

    Added missing semicolons.

--- features/support/world.js
+++ features/support/world.js
@@ -12,13 +12,14 @@ function addAssertionTextContains() {
 function add_parameters(url, parameters) {
   for (var i = 0; i < parameters.length; i++) {
     if (i === 0) {
-      url += '?'
+      url += '?';
     } else {
-      url += '&'
+      url += '&';
     }
-    url += parameters[i]['param']
-    url += '='
-    url += parameters[i]['value']
+    
+    url += parameters[i]['param'];
+    url += '=';
+    url += parameters[i]['value'];
   }
 
   return url;
@@ -33,7 +34,7 @@ function World() {
     var url = '/';
 
     if (parameters) {
-      url = add_parameters(url, parameters)
+      url = add_parameters(url, parameters);
     }
 
     this.browser.visit(url, callback);
