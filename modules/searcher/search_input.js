a082858144a5fc21ae13a2e61831a1851653c144
nwshane <django09@gmail.com>
2016-01-26 17:03:36 +0400

    Small method name change.

--- modules/searcher/search_input.js
+++ modules/searcher/search_input.js
@@ -17,7 +17,7 @@ let search_input = {
   },
 
   run_search: function(search_query, search_results_processor, search_results_container) {
-    this.change_q_param_in_url(search_query);
+    this.update_q_param_in_url(search_query);
 
     let response = this.send_search_query(search_query);
 
@@ -29,7 +29,7 @@ let search_input = {
     search_results_container.fill(processed_results);
   },
 
-  change_q_param_in_url: function(search_query) {
+  update_q_param_in_url: function(search_query) {
     let current_q_value = QueryString.q;
     if (current_q_value !== search_query) {
       let urlPath = '/?q=' + search_query;
